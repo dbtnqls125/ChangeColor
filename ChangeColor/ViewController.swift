9ef7579431caf0a82f86ea6ced4b2db36c77a677
D7703_18 <d7703_18@d7703-18ui-imac.local>
2019-03-12 16:01:30 +0900

    Change backgroundColor

--- ChangeColor/ViewController.swift
+++ ChangeColor/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var whiteColor: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func change_red(_ sender: Any) {
+        self.view.backgroundColor = UIColor.red
+        whiteColor.text = "Red"
+    }
+    
+    @IBAction func change_yellow(_ sender: Any) {
+        self.view.backgroundColor = UIColor.yellow
+        whiteColor.text = "Yellow"
+    }
+    
+    @IBAction func change_green(_ sender: Any) {
+        self.view.backgroundColor = UIColor.green
+        whiteColor.text = "Green"
+    }
 }
 
